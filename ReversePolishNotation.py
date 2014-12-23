d3d8ffcbd241e139f4654727559efcba35f4a15f
Denis Laliberté <denis@tp1.ca>
2014-12-23 13:25:38 -0500

    refactor to function

--- ReversePolishNotation.py
+++ ReversePolishNotation.py
@@ -1,11 +1,10 @@
 import operator
 
-class ReversePolishNotation():
+def ReversePolishNotation(input):
   op = {"+": operator.add,
         "*": operator.mul,
         "/": operator.div,
         "-": operator.sub}
-  def compute(self,input):
-    index = input[2]
-    operation = self.op[index]
-    return operation(input[0], input[1])
+  index = input[2]
+  operation = op[index]
+  return operation(input[0], input[1])
--- ReversePolishNotation_test.py
+++ ReversePolishNotation_test.py
@@ -5,20 +5,16 @@ from ReversePolishNotation import ReversePolishNotation
 
 class ReversePolishNotation_test(unittest.TestCase):
   def test(self):
-    rpn = ReversePolishNotation();
-    self.assertEqual(rpn.compute([1,1,'+']),2)
+    self.assertEqual(ReversePolishNotation([1,1,'+']),2)
 
   def test_multiplication(self):
-    rpn = ReversePolishNotation();
-    self.assertEqual(rpn.compute([2,3,'*']),6)
+    self.assertEqual(ReversePolishNotation([2,3,'*']),6)
 
   def test_division(self):
-    rpn = ReversePolishNotation();
-    self.assertEqual(rpn.compute([4,2,'/']),2)
+    self.assertEqual(ReversePolishNotation([4,2,'/']),2)
 
   def test_subtraction(self):
-    rpn = ReversePolishNotation();
-    self.assertEqual(rpn.compute([5,2,'-']),3)
+    self.assertEqual(ReversePolishNotation([5,2,'-']),3)
 
 #  def test_2_addition(self):
 #    rpn = ReversePolishNotation();
